abb671bff002ae8209d7d6ab23b2c91d84d7d648
Shelan <shelandebesai@gmail.com>
2019-08-12 16:47:21 -0700

    first commit

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+var insertionSort = function (arr) {
+    for(var i = 1; i < arr.length; i++){ //start loop at index 1
+        for(var j = 0; j<i; j++) {
+            if(arr[i] < arr[j]){
+                var temp = arr.splice(i,1)
+                arr.splice(j,0,temp[0])
+            }
+        }
+    }
+   return arr
+}
+let arr = [2,3,5,1,0]
+console.log(insertionSort(arr))
