6db7877575827c37367769c7fa6da11de624a075
Shelan <shelandebesai@gmail.com>
2019-08-13 10:40:42 -0700

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -1,9 +1,9 @@
 var insertionSort = function (arr) {
     for(var i = 1; i < arr.length; i++){ //start loop at index 1
         for(var j = 0; j<i; j++) {
-            if(arr[i] < arr[j]){
-                var temp = arr.splice(i,1) //store deleted element in temp
-                arr.splice(j,0,temp[0]) //swap
+            if(arr[i] < arr[j]){ //comparison 
+                var temp = arr.splice(i,1) //stores deleted element
+                arr.splice(j,0,temp[0]) //insertion of deleted element 
             }
         }
     }
@@ -11,3 +11,5 @@ var insertionSort = function (arr) {
 }
 let arr = [2,3,5,1,0]
 console.log(insertionSort(arr))
+
+//Time complexity 0(n^2) 
